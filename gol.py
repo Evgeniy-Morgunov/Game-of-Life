c526253f5f8df5a2f5cc8e576124f0dfb271c1b6
Evgeniy-Morgunov <94369750+Evgeniy-Morgunov@users.noreply.github.com>
2021-12-13 10:03:28 +0800

    Life with trace.

--- gol.py
+++ gol.py
@@ -3,69 +3,42 @@ import numpy as np
 import pygame
 import sys
 
+def compute_neighbours(Z, N):
 
-def compute_neighbours(Z, X):
-    # start_time = pygame.time.get_ticks()
-
-    N = np.zeros_like(Z)
-    M = np.zeros_like(Z)
-
-    N[1:-1,1:-1] = Z[:-2, :-2] + Z[1:-1, :-2] + Z[:-2, 1:-1] \
-                 + Z[2:,  :-2]                + Z[2:,  1:-1] \
-                 + Z[:-2,  2:] + Z[1:-1,  2:] + Z[2:,    2:]
-
-    M[1:-1,1:-1] = X[:-2, :-2] + X[1:-1, :-2] + X[:-2, 1:-1] \
-                 + X[2:,  :-2]                + X[2:,  1:-1] \
-                 + X[:-2,  2:] + X[1:-1,  2:] + X[2:,    2:]
-
-    # print("compute neighbours ", (pygame.time.get_ticks() - start_time)/1000)
-    return N, M, N + M
+    N[1:-1,1:-1] = Z[:-2,:-2] + Z[1:-1,:-2] + Z[:-2,1:-1] \
+                 + Z[2:,:-2]                + Z[2:,1:-1] \
+                 + Z[:-2,2:]  + Z[1:-1,2:]  + Z[2:,2:]
 
 
-def iterate(Z, X):
-    # start_time = pygame.time.get_ticks()
 
-    N, M, NM = compute_neighbours(Z, X)
-
-    Z[np.where(NM > 3)] = 0
-    Z[np.where(NM < 2)] = 0
-    Z[np.where((NM == 3)&(N > M))] = 1
-
-    X[np.where(NM > 3)] = 0
-    X[np.where(NM < 2)] = 0
-    X[np.where((NM == 3)&(M > N))] = 1
+def iterate(Z, N, M):
     
-    # print("iterate ", (pygame.time.get_ticks() - start_time)/1000)
+    birth = ((N == 3) & (Z == 0))
+    survive = ((N == 2) | (N == 3)) & (Z == 1) 
+    
+    Z[...] = 0
+    Z[birth | survive] = 1
+    
+    M[M > 0.25] = 0.25
+    M *= 0.995
+    M[Z == 1] = 1
+
 
 
 def draw_blocks(screen, xlen, ylen, world):
-    # start_time = pygame.time.get_ticks()
 
-    surf = pygame.surfarray.make_surface(world)
+    surf = pygame.surfarray.make_surface(world * 255)
     
     screen.blit(surf, (-1, -1))
     
-    # print("draw ", (pygame.time.get_ticks() - start_time)/1000)
 
 
 def make_random_grid(x, y):
-    Zgrid = np.random.randint(0, 2, size=(x, y))
-    Xgrid = np.random.randint(0, 2, size=(x, y))
-    Zgrid[:, 0] *= 0
-    Zgrid[:, -1] *= 0
-    Zgrid[0:x//2] *= 0
-    Zgrid[-1] *= 0
-    
-    Xgrid[:, 0] *= 0
-    Xgrid[:, -1] *= 0
-    Xgrid[x//2:] *= 0
-    Xgrid[0] *= 0
-    
-    return Zgrid, Xgrid
-
-
-def make_clear_grid(x, y):
-    grid = np.zeros((x, y))
+    grid = np.random.randint(0, 2, size=(x, y))
+    grid[:, 0] *= 0
+    grid[:, -1] *= 0
+    grid[0] *= 0
+    grid[-1] *= 0
     
     return grid
 
@@ -78,11 +51,16 @@ def main():
     clock = pygame.time.Clock()
 
     h = 0
-    xlen = xmax + 2
-    ylen = ymax + 2
-    Zworld, Xworld = make_random_grid(xlen, ylen)
+    scale = 1
+    xlen = xmax // scale + 2
+    ylen = ymax // scale + 2
+    world = make_random_grid(xlen, ylen)
+    all_world = np.zeros(world.shape)
+
     play = False
-    
+
+    N = np.zeros_like(world)
+
     while True:
         screen.fill("black")
 
@@ -90,16 +68,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 0
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     play = not play
 
-        draw_blocks(screen, xlen, ylen, (Zworld * 20) + (Xworld * 100))
-        
-        if play:
-            iterate(Zworld, Xworld)
+        draw_blocks(screen, xlen, ylen,np.stack((all_world, all_world, all_world), axis=2 ))
 
-        pygame.display.set_caption("$~GoL ~fps: " + str(round(clock.get_fps(), 2)))
+        compute_neighbours(world, N)
+
+        if play:
+            iterate(world, N, all_world)
+
+        pygame.display.set_caption("$~GoL ~fps: " + str(round(clock.get_fps(), 3)))
 
         pygame.display.flip()
         clock.tick()
